76345b336b45a86c2c47ce6992aece666e6dcee3
playerunner <90867096+playerunner@users.noreply.github.com>
2021-09-20 13:35:20 -0600

    Add files via upload

---
+++ Phone-book_mainpage.py
@@ -0,0 +1,37 @@
+import Phone_book_module
+
+print ()
+c1 = Phone_book_module.p1
+c2 = Phone_book_module.p2
+c3 = Phone_book_module.p3
+c4 = Phone_book_module.b1
+c5 = Phone_book_module.b2
+
+P1 = Phone_book_module.n1
+P2 = Phone_book_module.n2
+P3 = Phone_book_module.n3
+P4 = Phone_book_module.n4
+P5 = Phone_book_module.n5
+
+Ad1 = Phone_book_module.ad1
+Ad2 = Phone_book_module.ad2
+Ad3 = Phone_book_module.ad3
+Ad4 = Phone_book_module.ad4
+Ad5 = Phone_book_module.ad5
+
+lookup1 = (input("Who are you looking for "))
+
+if lookup1 == "c1":
+    print ("Here is your info", c1,P1,Ad1)
+elif lookup1 == "c2":
+    print ("Here is your info", c2,P2,Ad2)
+elif lookup1 == "c3":
+    print ("Here is your info", c3,P3,Ad3)
+elif lookup1 == "c4":
+    print ("Here is your info", c4,P4,Ad4)
+elif lookup1 == "c5":
+    print ("Here is your info", c5,P5,Ad5)
+else:
+    print("That contact does not exist")
+
+print ("Thank you for using this PhoneBook")
---
+++ Phone_book_module.py
@@ -0,0 +1,28 @@
+#module for phone book
+
+#names
+p1 = "Ruby Rose,"
+p2 = "Blake Belladonna,"
+p3 = "Weiss Schnee,"
+
+#business
+b1 = "Pizza 73,"
+b2 = "Edo Japan,"
+
+#numbers
+n1 = "(780)-345-8798,"
+n2 = "(780)-657-9870,"
+n3 = "(780)-765-8457,"
+
+#busniess numbers
+n4 = "(780)-473-7373,"
+n5 = "(780)-479-1372,"
+
+#addresses
+ad1 = "5016 118 Ave NW"
+ad2 = "5017 118 Ave Nw"
+ad3 = "5018 118 Ave NW"
+
+#busniess addresses
+ad4 = "5003 118 Ave NW"
+ad5 = "Kingsway Mall"
